9a70d8311fed983076c3d35da260828578e57574
JoLeeSoftwire <Jo.Lee@softwire.com>
2019-11-20 12:25:37 +0000

    Part 2

--- main.java
+++ main.java
@@ -2,25 +2,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 class EmailExtraction {
     public static void main(String[] args) throws IOException {
-        String emailString = "@softwire.com";
-        int len = emailString.length();
         int count = 0;
         Path path = Paths.get(args[0]);
         String input = Files.readString(path);
-        for(var i = 0; i<input.length(); i++) {
-            if(i+len < input.length()) {
-                if(input.substring(i, i+len).equals(emailString)) {
-                    count++;
-                }
-            } else {
-                if(input.substring(i).equals(emailString)) {
-                    count++;
-                }
-            }
+        Pattern pattern = Pattern.compile("[\\w.'_%+-]+@softwire.com");
+        Matcher matcher = pattern.matcher(input);
+        var hasMatch = true;
+        while(hasMatch) {
+            hasMatch = matcher.find();
+            if(hasMatch) {count ++; System.out.println(matcher.group());}
         }
+        
+
         System.out.println(count);
     }
 }
