45c4ca648e733cda5a0421d44e7f1252f7a96657
JoLeeSoftwire <Jo.Lee@softwire.com>
2019-11-20 14:01:00 +0000

    part 3 v1

--- main.java
+++ main.java
@@ -4,21 +4,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.HashMap;
 
 class EmailExtraction {
     public static void main(String[] args) throws IOException {
-        int count = 0;
+        HashMap<String, Integer> frequencies = new HashMap<String, Integer>();
         Path path = Paths.get(args[0]);
         String input = Files.readString(path);
-        Pattern pattern = Pattern.compile("[\\w.'_%+-]+@softwire.com");
+        Pattern pattern = Pattern.compile("\\s[\\w.'_%+-]+@(\\w+.\\w+)\\s");
         Matcher matcher = pattern.matcher(input);
         var hasMatch = true;
         while(hasMatch) {
             hasMatch = matcher.find();
-            if(hasMatch) {count ++; System.out.println(matcher.group());}
+            if(hasMatch) {
+                if(!frequencies.containsKey(matcher.group(1))) {
+                    frequencies.put(matcher.group(1), 1);
+                } else {
+                    frequencies.put(matcher.group(1), frequencies.get(matcher.group(1)) + 1);
+                }
+                // System.out.println(matcher.group());
+            }
         }
-        
-
-        System.out.println(count);
+        for(String i:frequencies.keySet()) {
+            System.out.print(i);
+            System.out.print(": ");
+            System.out.println(frequencies.get(i));
+        }
     }
 }
